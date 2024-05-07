c56caed3c23caa9588610ae62326683fbd6108dd
OmeletteX <chen.qixuan97@gmail.com>
2024-05-07 16:24:44 +0800

    “added_google_analytics”

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-33SM37MPCX");
+
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
